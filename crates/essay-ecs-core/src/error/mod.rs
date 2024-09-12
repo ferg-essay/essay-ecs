fd1791d0b26d65a377ee7ce4b9a44b92754af20e
Scott Ferguson <124411070+ferg-essay@users.noreply.github.com>
2024-09-12 09:51:26 -0700

    add error_loc

--- crates/essay-ecs-core/src/error/mod.rs
+++ crates/essay-ecs-core/src/error/mod.rs
@@ -1,8 +1,45 @@
+use std::error;
+
 pub struct Error {
-    msg: String
+    msg: String,
+    _source: Option<Box<dyn error::Error + Send + Sync>>
 }
 
 impl Error {
+    #[inline]
+    pub fn new(msg: &str) -> Self {
+        Error {
+            msg: msg.to_string(),
+            _source: None,
+        }
+    }
+
+    #[inline]
+    pub fn other<E>(error: E) -> Self
+    where 
+        E: Into<Box<dyn error::Error + Send + Sync>>
+    {
+        let error = error.into();
+
+        Error {
+            msg: format!("{}", error),
+            _source: Some(error),
+        }
+    }
+
+    #[inline]
+    pub fn other_loc<E>(error: E, loc: &str) -> Self
+    where 
+        E: Into<Box<dyn error::Error + Send + Sync>>
+    {
+        let error = error.into();
+
+        Error {
+            msg: format!("{}\n\tat {}", error, loc),
+            _source: Some(error),
+        }
+    }
+
     #[inline]
     pub fn message(&self) -> &str {
         &self.msg
@@ -12,7 +49,8 @@ impl Error {
 impl From<&str> for Error {
     fn from(value: &str) -> Self {
         Self {
-            msg: value.to_string()
+            msg: value.to_string(),
+            _source: None,
         }
     }
 }
@@ -20,8 +58,16 @@ impl From<&str> for Error {
 impl From<String> for Error {
     fn from(value: String) -> Self {
         Self {
-            msg: value
+            msg: value,
+            _source: None,
         }
+    }
+}
+
+impl std::fmt::Display for Error {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.msg)
     }
 }
 
@@ -33,3 +79,27 @@ impl std::fmt::Debug for Error {
 }
 
 pub type Result<V, E=Error> = std::result::Result<V, E>;
+
+#[allow(unused_macros)]
+macro_rules! error_loc {
+    ($($param:expr),*) => {
+        $crate::error::Error::new(&format!("{} in {}\n\tat {}:{}:{}", 
+            format_args!($($param,)*), 
+            module_path!(),
+            file!(), 
+            line!(), 
+            column!()
+        ))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_error_log() {
+        assert_eq!(
+            "My message 13 in essay_ecs_core::error::test\n\tat crates/essay-ecs-core/src/error/mod.rs:102:13",
+            error_loc!("My message {}", 13).message(),
+        );
+    }
+}
